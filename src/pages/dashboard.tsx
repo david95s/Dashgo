d10c7f0cbd754c7e10fa25a9dfdcb524c393a019
david <davidlinconl1@hotmail.com>
2021-11-17 15:09:53 -0300

    Added Lib apexcharts

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,17 @@
-//dashboard.tsx
-import { Flex } from "@chakra-ui/react";
+import { Flex, SimpleGrid, Box, Text } from "@chakra-ui/react";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/SideBar";
+import Chart from 'react-apexcharts';
+
+const options = {};
+
+const series = [
+  {
+    name: 'series',
+    data: [31,120,10, 28, 61,18, 109]
+  }
+]
+
 
 export default function Dashboard() {
   return (
@@ -9,8 +19,24 @@ export default function Dashboard() {
       <Header/>
       <Flex w="100%" my="6" maxW={ 1480 } mx="auto" px="6">
         <Sidebar/>
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+          > 
+            <Text fontSize="lg" mb="4">Inscritos da Semana</Text>
+              <Chart options={options} series={series} type="area" height={ 160 }/>
+          </Box>
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+          > 
+            <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
-  )
-
+  );
 }
